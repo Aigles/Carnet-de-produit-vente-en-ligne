46bd074b6c6a20818cf9aed079d83eabd40fbe3f
Yves Dalien Jean <doudoujean32@gmail.com>
2018-04-08 18:42:04 +1200

    Debug sur la liste des vetements et mise a jour de la base de donnees

--- 13orcollection/themes/js/list.js
+++ 13orcollection/themes/js/list.js
@@ -19,20 +19,19 @@ if(page == '' || page == 'index.php'){
                 })
     .done(function(data) { 
   
-     var itemsProduit = [];
+    var tableau_produit="";
     
      $.each( data, function( key, produit ) {
 
-     tableau_produit='<li class="span3"><div class="thumbnail"><a  href="product_details.html"><img class="taille-img" src="themes/images/products/sweater.jpg" alt=""/></a>';
-     tableau_produit+='<div class="caption"><h5>'+produit.nom+'</h5> <p>'+produit.description+'. </p><h4 style="text-align:center"><a  href="product_details.html?prodid='+produit.id+' class="btn btn-warning">Ajouter <i class="icon-shopping-cart"></i></a> <a class="btn btn-warning" href="href="product_details.html?prodid='+produit.id+'">voir</a></h4>';
+     tableau_produit+='<li class="span3"><div class="thumbnail"><a  href="product_details.html"><img class="taille-img" src="themes/images/products/sweater.jpg" alt=""/></a>';
+     tableau_produit+='<div class="caption"><h5>'+produit.nom+'</h5> <p>'+produit.description+'. </p><h4 style="text-align:center"><a  href="product_details.html?prodid='+produit.id+'" class="btn btn-warning">Ajouter <i class="icon-shopping-cart"></i></a> <a class="btn btn-warning" href="product_details.html?prodid='+produit.id+'">voir</a></h4>';
       tableau_produit+='</div></div></li>';
 
      
-     itemsProduit.push(tableau_produit);
+     //itemsProduit.push(tableau_produit);
  
     });
-     document.getElementById('List_produit').innerHTML=itemsProduit;
-
+     document.getElementById('List_produit').innerHTML=tableau_produit;
   
     });
 
---
+++ auto_vente_en_ligne.bat
@@ -0,0 +1,5 @@
+git pull
+git add -A
+git status
+git commit -m "Debug sur la liste des vetements et mise a jour de la base de donnees"
+git push
