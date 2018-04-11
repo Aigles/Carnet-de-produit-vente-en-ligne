35f1b817fe23ae9c2c7e4e77614c3452bc3dd5e7
bellune <bellunetabithamegane@gmail.com>
2018-04-11 12:50:31 -0400

    Arrangermerge

--- 13orcollection/themes/js/detail.js
+++ 13orcollection/themes/js/detail.js
@@ -1,31 +1,27 @@
-  
+   
     var path = window.location.pathname;
     var page = path.split("/").pop();
-   // var id = GET_PARAM('pid');
 
 
     //List personnel all records
-    if(page == '' || page == 'product_details.php'){
+    if(page == '' || page == 'product_details.php?prodid='+produit.id+''){
 
     
 
     $.ajax({ 
-            url: Fullurl+"produit/",
+            url: Fullurl+"produit",
             type: 'GET', 
             dataType: 'json', 
             Accept : "application/json;charset=UTF-8"
             })
 
-    
     .done(function(data) { 
         var tableau_produit="";
     
-        $.each( data, function( key, produit ){
-         tableau_produit+='<li class="span3"><div class="thumbnail"><a  href="product_details.php"><img class="taille-img" src="themes/images/products/sweater.jpg" alt=""/></a>';
-         tableau_produit+='<div class="caption"><h5>'+produit.nom+'</h5> <p>'+produit.description+'. </p><p>Prix : '+produit.prix+'. </p><h4 style="text-align:center"><a  href="cart.php?prodid='+produit.id+'" class="btn btn-warning">Ajouter <i class="icon-shopping-cart"></i></a> <a class="btn btn-warning" href="product_details.php?prodid='+produit.id+'">voir</a></h4>';
-         tableau_produit+='</div></div></li>';
+
+         tableau_produit+='<h3> '+produit.nom+' </h3>';
         
-         });
+        
      // affectation des produits dans l'id
      document.getElementById('detail_produit').innerHTML=tableau_produit;
     
