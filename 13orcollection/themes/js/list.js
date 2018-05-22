1c689c2de669d0d98ce5379b6fbad40a91284170
YvesDalienjean <doudoujean32@gmail.com>
2018-05-22 04:19:13 -0700

    arrangement de la page utilisateur

--- 13orcollection/themes/js/list.js
+++ 13orcollection/themes/js/list.js
@@ -42,7 +42,7 @@ function GET_PARAM(param) { var vars = {}; window.location.href.replace( locatio
             if (produit.caracteristic != null) {
                 var tableau_produit="";
                  tableau_produit+='<li class="span3"><div class="thumbnail"><a  href="product_details.php?pid='+produit.id+'"><img class="taille-img" src="'+produit.caracteristic[0].image+'" alt=""/></a>';
-                 tableau_produit+='<div class="caption"><h5>'+produit.nom+'</h5> <p>En stock: '+produit.nbre_en_stock+'. </p><p>Prix : $'+produit.caracteristic[0].prix+',00 </p><h4 style="text-align:center"><a  href="cart.php" class="btn btn-warning  ajouter-panier"  data-image="'+produit.caracteristic[0].image+'" data-nom="'+produit.nom+'" data-prix="'+produit.caracteristic[0].prix+'">Ajouter <span class="icon-shopping-cart"></span> </a> <button type="button" href="'+produit.id+'" class="btn btn-sm btn-warning" onclick="detailsmodal('+produit.id+');"><i class="icon-zoom-in"></i></button></h4>';
+                 tableau_produit+='<div class="caption"><h5>'+produit.nom+'</h5> <p>En stock: '+produit.nbre_en_stock+'. </p><p>Prix : $'+produit.caracteristic[0].prix+',00 </p><h4 style="text-align:center"><a href="cart.php" class="btn btn-warning  ajouter-panier"  data-image="'+produit.caracteristic[0].image+'" data-nom="'+produit.nom+'" data-prix="'+produit.caracteristic[0].prix+'">Ajouter <span class="icon-shopping-cart"></span> </a> <button type="button" href="'+produit.id+'" class="btn btn-sm btn-warning" onclick="detailsmodal('+produit.id+');"><i class="icon-zoom-in"></i></button></h4>';
                  tableau_produit+='</div></div></li>';
 
                 jQuery('#List_produit').append(tableau_produit);
@@ -148,7 +148,6 @@ $.ajax({
     var image =$(this).data('image'); 
     MonPanier.ajouter_produit_dans_panier(nom, prix,qte,image);
     afficherpanier();
-    location.window="cart.php";
 });
  // affectation des produits dans l'id
  //document.getElementById('List_produit').innerHTML=tableau_produit;
--- 13orcollection/themes/js/listerRecherche.js
+++ 13orcollection/themes/js/listerRecherche.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
   var expression = new RegExp(searchField, "i");
   $.getJSON('http://localhost:1230/api/app/produit', function(data) {
    $.each(data, function(key, value){
-    if (value.nom.search(expression) != -1)
+    if (value.nom.search(expression)!= -1)
     {
-     $('#result').append('<li class="list-group-item link-class"><a href="product_details.php?pid='+value.id+'"><img src="'+value.caracteristic[0].image+'" height="40" width="40" class="img-thumbnail" /> '+value.nom+' | <span class="text-muted"> ($'+value.caracteristic[0].prix+'.00)</span></a></li>');
+     $('#result').append('<li class="list-group-item link-class"><a href="product_details.php?pid='+value.id+'"><img src="'+value.caracteristic[0].image+'" height="30" width="30" class="img-thumbnail" /> '+value.nom+' | <span class="text-muted"> ($'+value.caracteristic[0].prix+'.00)</span></a></li>');
     }
    });   
   });
