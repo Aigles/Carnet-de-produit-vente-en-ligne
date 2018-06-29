d5a3c498a4a99110935e9b250b7ff0823d04c8fb
bellune <bellunetabithamegane@gmail.com>
2018-06-29 12:06:55 -0400

    Quelque petite modification

--- API/src/Configuration/config.go
+++ API/src/Configuration/config.go
@@ -1,5 +1,5 @@
 package Configuration
 var base_de_donnees="aigle_vente_en_ligne";
 var utilisateur="root";
-var password="";
-//var password="R3zDUaX%+vd";
+//var password="";
+var password="R3zDUaX%+vd";
