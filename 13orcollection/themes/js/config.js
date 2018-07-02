545930c91c0a6a7e866433f9fde1c130355ff7c0
bellune <bellunetabithamegane@gmail.com>
2018-07-02 13:42:13 -0400

    mise en place de l'api

--- 13orcollection/themes/js/config.js
+++ 13orcollection/themes/js/config.js
@@ -1,1 +1,2 @@
-var Fullurl="http://192.168.43.27:1230/api/app/";
+//var Fullurl="http://192.168.43.27:1230/api/app/";
+var Fullurl="http://207.246.123.38/~aigle/Vente-en-ligne/API/vente_en_ligne_API";
