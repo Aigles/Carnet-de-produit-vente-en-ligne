d5f47b18949438443470035c2138e000d4a04a7c
bellune <bellunetabithamegane@gmail.com>
2018-07-02 23:36:40 -0400

    mise a jour du module paiment

--- 13orcollection/paypal/index.php
+++ 13orcollection/paypal/index.php
@@ -40,8 +40,8 @@
 	$payment->setIntent('sale');
 
 	$redirectUrls = (new \PayPal\Api\RedirectUrls())
-	    ->setReturnUrl('http://207.246.123.38/~aigle/public/Vente_en_linge/13orcollection/index.php?p=success')
-	    ->setCancelUrl('http://207.246.123.38/~aigle/public/Vente_en_linge/13orcollection/index.php');
+	    ->setReturnUrl('http://207.246.123.38/~aigle/Vente-en-ligne/13orcollection/index.php?p=success')
+	    ->setCancelUrl('http://207.246.123.38/~aigle/Vente-en-ligne/13orcollection/index.php');
 
 	$payment->setRedirectUrls($redirectUrls);
 
